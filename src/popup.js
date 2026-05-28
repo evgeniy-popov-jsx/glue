951cf78e950a2e9dd88d0c4d472929cdaa79e137
unknown <evgeniy.popov.jsx@gmail.com>
2026-05-28 19:05:49 +0300

    init

---
+++ src/popup.js
@@ -0,0 +1,21 @@
+const popup = document.getElementById('popup');
+const popupClose = document.getElementById('popupClose');
+const button = document.getElementById('button');
+
+function openPopup() {
+  console.log('klik');
+  popup.classList.add('active');
+}
+
+function closePopup() {
+  popup.classList.remove('active');
+}
+
+popupClose.addEventListener('click', closePopup);
+button.addEventListener('click', openPopup);
+
+popup.addEventListener('click', (e) => {
+  if (e.target.classList.contains('popup__overlay')) {
+    closePopup();
+  }
+});
