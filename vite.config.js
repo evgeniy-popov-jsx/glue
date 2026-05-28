4fff9800622fd78a8d5d98feeb4935753a887f68
unknown <evgeniy.popov.jsx@gmail.com>
2026-05-28 19:41:59 +0300

    fix3

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/glue/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        privacy: 'glue/privacy/index.html',
+        terms: 'glue/terms/index.html',
+      },
+    },
+  },
 });
